82c7967350b08f5a0352b84cbc2f8b3a041d8241
EdoardoDini <edoardodini94@gmail.com>
2021-08-08 16:13:01 +0200

    Created the two models that will be used in the application

---
+++ src/main/java/com/example/suggestionProject/model/Comment.java
@@ -0,0 +1,58 @@
+package com.example.suggestionProject.model;
+
+import java.util.Objects;
+
+public class Comment {
+
+	private Long commentId;
+	private String commentText;
+	private Suggestion suggestion;
+
+	public Long getCommentId() {
+		return commentId;
+	}
+
+	public void setCommentId(Long commentId) {
+		this.commentId = commentId;
+	}
+
+	public String getCommentText() {
+		return commentText;
+	}
+
+	public void setCommentText(String commentText) {
+		this.commentText = commentText;
+	}
+
+	public Suggestion getSuggestion() {
+		return suggestion;
+	}
+
+	public void setSuggestion(Suggestion suggestion) {
+		this.suggestion = suggestion;
+	}
+
+	@Override
+	public String toString() {
+		return "Comment [commentId=" + commentId + ", commentText=" + commentText + ", suggestion=" + suggestion + "]";
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(commentId, commentText, suggestion);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Comment other = (Comment) obj;
+		return Objects.equals(commentId, other.commentId) && Objects.equals(commentText, other.commentText)
+				&& Objects.equals(suggestion, other.suggestion);
+	}
+
+}
---
+++ src/main/java/com/example/suggestionProject/model/Suggestion.java
@@ -0,0 +1,64 @@
+package com.example.suggestionProject.model;
+
+import java.util.Objects;
+
+public class Suggestion {
+
+	private Long id;
+	private String suggestionText;
+	private Boolean visible;
+
+	public Suggestion(Long id, String suggestionText, boolean visible) {
+		this.id = id;
+		this.suggestionText = suggestionText;
+		this.visible = visible;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getSuggestionText() {
+		return suggestionText;
+	}
+
+	public void setSuggestionText(String suggestion) {
+		this.suggestionText = suggestion;
+	}
+
+	public Boolean getVisible() {
+		return visible;
+	}
+
+	public void setVisible(Boolean visible) {
+		this.visible = visible;
+	}
+
+	@Override
+	public String toString() {
+		return "Suggestion [id=" + id + ", suggestionText=" + suggestionText + ", visible=" + visible + "]";
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id, suggestionText, visible);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Suggestion other = (Suggestion) obj;
+		return Objects.equals(id, other.id) && Objects.equals(suggestionText, other.suggestionText)
+				&& Objects.equals(visible, other.visible);
+	}
+
+}
