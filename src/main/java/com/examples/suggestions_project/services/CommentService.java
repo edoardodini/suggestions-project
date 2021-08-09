63523cea1a859683ef3764565a065ad5eae47455
EdoardoDini <edoardodini94@gmail.com>
2021-08-09 23:57:26 +0200

    Completed the first method in CommentService

--- src/main/java/com/examples/suggestions_project/services/CommentService.java
+++ src/main/java/com/examples/suggestions_project/services/CommentService.java
@@ -3,12 +3,17 @@ package com.examples.suggestions_project.services;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import com.examples.suggestions_project.repository.SuggestionRepository;
+import com.examples.suggestions_project.model.Comment;
+import com.examples.suggestions_project.repository.CommentRepository;
 
 @Service
 public class CommentService {
 
 	@Autowired
-	private SuggestionRepository suggestionRep;
+	private CommentRepository commentRepository;
+	
+	public Comment getCommentById(long id) {
+		return commentRepository.findById(id).orElse(null);
+	}
 
 }
--- src/test/java/com/examples/suggestions_project/services/CommentServiceTest.java
+++ src/test/java/com/examples/suggestions_project/services/CommentServiceTest.java
@@ -1,23 +1,17 @@
 package com.examples.suggestions_project.services;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.mockito.Mockito.*;
-import static org.mockito.AdditionalAnswers.returnsFirstArg;
 
-import static java.util.Arrays.*;
 import java.util.Optional;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.junit.MockitoJUnitRunner;
-import org.springframework.dao.EmptyResultDataAccessException;
 
-import com.examples.suggestions_project.exception.ResourceNotFoundException;
 import com.examples.suggestions_project.model.Comment;
 import com.examples.suggestions_project.model.Suggestion;
 import com.examples.suggestions_project.repository.CommentRepository;
-import com.examples.suggestions_project.repository.SuggestionRepository;
 
 import org.mockito.InOrder;
 import org.mockito.InjectMocks;
@@ -40,6 +34,17 @@ public class CommentServiceTest {
 		Optional<Comment> comment = Optional.of(insideOfOptional);
 		when(commentRepository.findById(1)).thenReturn(comment);
 		assertThat(commentService.getCommentById(1)).isEqualTo(insideOfOptional);
+		InOrder inOrder = inOrder(commentRepository);
+		inOrder.verify(commentRepository).findById(idToFind);
+		inOrder.verifyNoMoreInteractions();
+	}
+	
+	@Test
+	public void testGetCommentByIdWithAnEmptyOptional() {
+		Long idToFind = 1L;
+		Optional<Comment> comment = Optional.empty();
+		when(commentRepository.findById(idToFind)).thenReturn(comment);
+		assertThat(commentService.getCommentById(idToFind)).isNull();
 		InOrder inOrder = inOrder(commentRepository);
 		inOrder.verify(commentRepository).findById(idToFind);
 		inOrder.verifyNoMoreInteractions();
