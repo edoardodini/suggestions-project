dc7bebecd05cc4d4e6bacdb7d96f664d009bbccf
EdoardoDini <edoardodini94@gmail.com>
2021-08-09 10:38:51 +0200

    Started the creation of tests for services

---
+++ src/test/java/com/examples/suggestions_project/services/SuggestionServiceTest.java
@@ -0,0 +1,25 @@
+package com.examples.suggestions_project.services;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.junit.MockitoJUnitRunner;
+
+import com.examples.suggestions_project.model.Suggestion;
+
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+
+@RunWith(MockitoJUnitRunner.class)
+public class SuggestionServiceTest {
+
+	@Mock
+	private SuggestionRepository suggestionRepository;
+	
+	@InjectMocks
+	private SuggestionService suggestionService;
+	
+	@Test
+	public void testGetSuggestionById() {
+		
+	}
+}
