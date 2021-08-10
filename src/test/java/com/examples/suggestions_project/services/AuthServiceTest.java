17fc551efec9ea99b168b02e08c318e8c7c1aa91
EdoardoDini <edoardodini94@gmail.com>
2021-08-10 19:00:40 +0200

    Small Refactoring of AuthServiceTest
    Changed the runner, setted a more correct ContextConfiguration and used
    Autowired to create a AuthService in the test.

--- src/test/java/com/examples/suggestions_project/services/AuthServiceTest.java
+++ src/test/java/com/examples/suggestions_project/services/AuthServiceTest.java
@@ -4,15 +4,17 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.context.ContextConfiguration;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.junit4.SpringRunner;
 
-@RunWith(SpringJUnit4ClassRunner.class)
-@ContextConfiguration
+@RunWith(SpringRunner.class)
+@ContextConfiguration(classes=AuthService.class)
 public class AuthServiceTest {
 	
-	private AuthService authenticationService = new AuthService();
+	@Autowired
+	private AuthService authenticationService;
 	
 	@Test
 	@WithMockUser(username="admin",roles={"admin"})
