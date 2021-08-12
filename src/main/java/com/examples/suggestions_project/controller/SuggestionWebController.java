66ba84973c1d1034e3098f12a61264803fa279b7
EdoardoDini <edoardodini94@gmail.com>
2021-08-12 12:39:55 +0200

    Implemented in SuggestionWebController the web endpoint
    the endpoint is created, SuggestionWebController is annotated as
    @Controller and the "home.html" file to return is created

--- src/main/java/com/examples/suggestions_project/controller/SuggestionWebController.java
+++ src/main/java/com/examples/suggestions_project/controller/SuggestionWebController.java
@@ -1,5 +1,14 @@
 package com.examples.suggestions_project.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class SuggestionWebController {
 
+	@GetMapping("/")
+	public String home(Model model) {
+		return "home";
+	}
 }
