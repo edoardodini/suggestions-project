00b4a4825ff8345575170e3d5ff8e615f191e223
EdoardoDini <edoardodini94@gmail.com>
2021-08-12 13:35:38 +0200

    Added a new test to check the presence of "suggestions" attribute
    Created also a fake implementation of SuggestionService to be mocked to
    write the test

--- src/main/java/com/examples/suggestions_project/controller/SuggestionWebController.java
+++ src/main/java/com/examples/suggestions_project/controller/SuggestionWebController.java
@@ -11,7 +11,7 @@ public class SuggestionWebController {
 	public String home(Model model) {
 		return "home";
 	}
-	
+
 	@GetMapping("/suggestions")
 	public String index(Model model) {
 		return "suggestionView";
--- src/test/java/com/examples/suggestions_project/controller/SuggestionWebControllerTest.java
+++ src/test/java/com/examples/suggestions_project/controller/SuggestionWebControllerTest.java
@@ -1,15 +1,25 @@
 package com.examples.suggestions_project.controller;
 
+import static java.util.Arrays.asList;
+import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.model;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
+
+import java.util.List;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.ModelAndViewAssert;
 import org.springframework.test.web.servlet.MockMvc;
+
+import com.examples.suggestions_project.model.Suggestion;
+import com.examples.suggestions_project.services.SuggestionService;
 
 @RunWith(SpringRunner.class)
 @WebMvcTest(controllers = SuggestionWebController.class)
@@ -17,6 +27,8 @@ public class SuggestionWebControllerTest {
 
 	@Autowired
 	private MockMvc mvc;
+	@MockBean
+	SuggestionService suggestionService;
 
 	@Test
 	public void testStatus200() throws Exception {
@@ -35,6 +47,17 @@ public class SuggestionWebControllerTest {
 
 	@Test
 	public void testReturnSuggestionView() throws Exception {
-		ModelAndViewAssert.assertViewName(mvc.perform(get("/suggestions")).andReturn().getModelAndView(), "suggestionView");
+		ModelAndViewAssert.assertViewName(mvc.perform(get("/suggestions")).andReturn().getModelAndView(),
+				"suggestionView");
+	}
+
+	@Test
+	public void testSuggestionViewShowsSuggestions() throws Exception {
+		List<Suggestion> suggestions = asList(new Suggestion(1L, "suggestionText", true));
+
+		when(suggestionService.getAllSuggestionsByVisible(true)).thenReturn(suggestions);
+
+		mvc.perform(get("/suggestions")).andExpect(view().name("index"))
+				.andExpect(model().attribute("employees", suggestions));
 	}
 }
