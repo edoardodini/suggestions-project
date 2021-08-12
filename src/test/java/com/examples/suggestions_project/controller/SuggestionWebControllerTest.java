0f60bd7006d84e381527dcabdae74f21b19459da
EdoardoDini <edoardodini94@gmail.com>
2021-08-12 12:52:21 +0200

    A test to check the view name and a failing test for a new web endpoint

--- src/test/java/com/examples/suggestions_project/controller/SuggestionWebControllerTest.java
+++ src/test/java/com/examples/suggestions_project/controller/SuggestionWebControllerTest.java
@@ -8,6 +8,7 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.ModelAndViewAssert;
 import org.springframework.test.web.servlet.MockMvc;
 
 @RunWith(SpringRunner.class)
@@ -21,4 +22,17 @@ public class SuggestionWebControllerTest {
 	public void testStatus200() throws Exception {
 		mvc.perform(get("/")).andExpect(status().is2xxSuccessful());
 	}
+	
+	@Test
+	public void testReturnHomeView() throws Exception {
+		ModelAndViewAssert.assertViewName(mvc.perform(get("/"))
+			.andReturn()
+			.getModelAndView(), "home");
+	}
+	
+	@Test
+	public void testStatus200Suggestions() throws Exception {
+		mvc.perform(get("/suggestions")).andExpect(status().is2xxSuccessful());
+	}
+	
 }
