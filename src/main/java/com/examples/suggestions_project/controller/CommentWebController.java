935c015b0e953a29914c4d66359f0fbc76c8e15a
EdoardoDini <edoardodini94@gmail.com>
2021-08-13 19:08:51 +0200

    Updated CommentWebController to pass the test

--- src/main/java/com/examples/suggestions_project/controller/CommentWebController.java
+++ src/main/java/com/examples/suggestions_project/controller/CommentWebController.java
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.examples.suggestions_project.model.Comment;
 import com.examples.suggestions_project.model.Suggestion;
+import com.examples.suggestions_project.services.AuthService;
 import com.examples.suggestions_project.services.CommentService;
 import com.examples.suggestions_project.services.SuggestionService;
 
@@ -25,15 +26,19 @@ public class CommentWebController {
 	private CommentService commentService;
 	@Autowired
 	private SuggestionService suggestionService;
-	
+	@Autowired
+	private AuthService authService;
 
 	@GetMapping("/comments")
 	public String home(Model model, @PathVariable Long suggestionId) {
 		List<Comment> commentsBySuggestionId = commentService.getCommentsBySuggestionId(suggestionId);
 		Suggestion suggestionById;
 		suggestionById = suggestionService.getSuggestionById(suggestionId);
-		model.addAttribute(USER_ATTRIBUTE, "admin");
-		model.addAttribute(COMMENTS_ATTRIBUTE, commentsBySuggestionId);
+		if (authService.isAdmin()) {
+			model.addAttribute(USER_ATTRIBUTE, "admin");
+		}else {
+			model.addAttribute(USER_ATTRIBUTE, "");
+		}model.addAttribute(COMMENTS_ATTRIBUTE, commentsBySuggestionId);
 		model.addAttribute(SUGGESTION_ATTRIBUTE, suggestionById);
 		return "commentView";
 	}
