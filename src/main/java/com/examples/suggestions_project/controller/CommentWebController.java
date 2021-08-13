54c8738a468ac2e417f1818531a63b4b9bffc7a2
EdoardoDini <edoardodini94@gmail.com>
2021-08-13 18:53:05 +0200

    Implemented the logic to pass the test
    Created also a fake implementation of CommentService to be mocked

--- src/main/java/com/examples/suggestions_project/controller/CommentWebController.java
+++ src/main/java/com/examples/suggestions_project/controller/CommentWebController.java
@@ -1,16 +1,38 @@
 package com.examples.suggestions_project.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.examples.suggestions_project.model.Comment;
+import com.examples.suggestions_project.model.Suggestion;
+import com.examples.suggestions_project.services.CommentService;
+import com.examples.suggestions_project.services.SuggestionService;
+
 @Controller
-@RequestMapping("suggestions/{id}")
+@RequestMapping("suggestions/{suggestionId}")
 public class CommentWebController {
 
+	private static final String SUGGESTION_ATTRIBUTE = "suggestion";
+	private static final String COMMENTS_ATTRIBUTE= "comments";
+	@Autowired
+	private CommentService commentService;
+	@Autowired
+	private SuggestionService suggestionService;
+	
+
 	@GetMapping("/comments")
-	public String home(Model model) {
+	public String home(Model model, @PathVariable Long suggestionId) {
+		List<Comment> commentsBySuggestionId = commentService.getCommentsBySuggestionId(suggestionId);
+		Suggestion suggestionById;
+		suggestionById = suggestionService.getSuggestionById(suggestionId);
+		model.addAttribute(COMMENTS_ATTRIBUTE, commentsBySuggestionId);
+		model.addAttribute(SUGGESTION_ATTRIBUTE, suggestionById);
 		return "commentView";
 	}
 
---
+++ src/main/java/com/examples/suggestions_project/services/CommentService.java
@@ -0,0 +1,17 @@
+package com.examples.suggestions_project.services;
+
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import com.examples.suggestions_project.model.Comment;
+
+@Service
+public class CommentService {
+	
+	private static final String TEMPORARY_IMPLEMENTATION = "Temporary implementation";
+
+	public List<Comment> getCommentsBySuggestionId(Long suggestionId) {
+		throw new UnsupportedOperationException(TEMPORARY_IMPLEMENTATION);
+	}
+}
