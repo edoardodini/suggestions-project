52e03fda7b6999cd86c82118facb84a40d5e68db
EdoardoDini <edoardodini94@gmail.com>
2021-08-29 22:34:47 +0200

    Various adjustment to made LoginWebControllerIT test pass
    Updated WebSecurityConfig to configure the login/logout mechanism
    Created the application.properties file for the test: actually it is
    equal to its related copy for the main application but the admin
    username, password and role is configured
    Updated also .classpath and org.eclipse.core.resources.prefs that are
    eclipse files who keep track of the insertion in the project of the new
    folder source "src/test/resources"

--- src/main/java/com/examples/suggestions_project/WebSecurityConfig.java
+++ src/main/java/com/examples/suggestions_project/WebSecurityConfig.java
@@ -10,6 +10,7 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
-		http.authorizeRequests().antMatchers("/api/**").permitAll();
+		http.authorizeRequests().antMatchers("/api/**").permitAll().and().formLogin().loginPage("/login").permitAll()
+				.and().logout().permitAll();
 	}
 }
